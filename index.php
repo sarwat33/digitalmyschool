1587f6f78ff7516c92c92956bb0f363e6d65cdce
Sarwat Razzaque Chy <73898320+sarwat33@users.noreply.github.com>
2021-11-21 23:56:44 -1100

    Update index.php

--- index.php
+++ index.php
@@ -48,7 +48,7 @@ function test_input($data) {
 }
 ?>
 
-<h2>PHP Form Validation Example</h2>
+  <h3>Simple functional form</h3>
 <p><span class="error">* required field</span></p>
 <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
   Name: <input type="text" name="name" value="<?php echo $name;?>">
